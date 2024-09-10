776308e8308accb97cd4115a1547b440b3f0422e
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-09-10 15:24:49 +0300

    test for animation features

--- src/app/[lng]/_components/ElgiwarFeatures.jsx
+++ src/app/[lng]/_components/ElgiwarFeatures.jsx
@@ -6,10 +6,17 @@ import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useTranslation } from '../../i18n/clinet';
-
+import { motion } from "framer-motion";
 import "./style.css";
 
 function ElgiwarFeatures({ lng }) {
+     const AnimationHeading = {
+    hidden: { opacity: 0, x: "-25vw" },
+    visible: { opacity: 1, x: 0 },
+    right: { opacity: 0, x: "25vw" },
+     bottom:{opacit:0,y:"25vh"},
+     visible2:{opacity:1,y:0}     
+  };
     const { t } = useTranslation(lng, 'home');
     const Featuers = t("Home.Features", { returnObjects: true })[0];
 
@@ -25,22 +32,39 @@ function ElgiwarFeatures({ lng }) {
     const restOfText = contactText.slice(firstTwoWords.length).trim();
 
     return (
-        <div className="ElgiwarFeatures pt-8">
-            <div className="containerr w-[90%] m-auto">
-                <div className="title text-center text-[#03245a] text-4xl pb-4 font-extrabold">
+        <div className="ElgiwarFeatures pt-16">
+            <div className="containerr overflow-hidden w-[90%] m-auto">
+                  <motion.h2
+        initial="hidden"
+        whileInView="visible"  
+        viewport={{ once: false }}
+        variants={AnimationHeading}
+        transition={{ duration: 1 }} 
+                   className="title text-center text-[#03245a] text-4xl pb-4 font-extrabold">
                     {Featuers.Heading}
-                </div>
-                <div className="title text-center text-[#03245a] text-xl pb-9 font-semibold">
+                </motion.h2>
+                          <motion.h2
+        initial="right"
+        whileInView="visible"  
+        viewport={{ once: false }}
+        variants={AnimationHeading}
+        transition={{ duration: 1 }}
+                    className="title text-center text-[#03245a] text-xl pb-9 font-semibold">
                     {Featuers.offer}
-                </div>
-                <div className="title text-center text-xl text-[#03245a]">
+                </motion.h2>
+                                  <motion.div
+        initial="bottom"
+        whileInView="visible2"  
+        viewport={{ once: false }}
+        variants={AnimationHeading}
+        transition={{ duration: 1 }} className="title text-center text-xl text-[#03245a]">
                    <a href="tel:+201094487922" className="underline  "> {firstTwoWords} </a>
                    <span>
                        {restOfText}
                    </span>
                  <span></span>
 
-                </div>
+                </motion.div>
 
                 <div className="elgiwar-works grid gap-4  mb-10 mt-12">
                     <div className="box1 grid grid-cols-[repeat(auto-fit,minmax(0,1fr))] gap-4">

21f0f0ed54046ab2bdba5b94a80d32333e6092cb
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-09-16 12:47:43 +0300

    fix all confilcts pages

--- src/app/[lng]/(company)/hotelBookings/page.jsx
+++ src/app/[lng]/(company)/hotelBookings/page.jsx
@@ -1,6 +1,8 @@
 
 
 import React from 'react';
+import { IoIosArrowForward } from "react-icons/io";
+
 import { useTranslation } from '../../../i18n'
 import Link from 'next/link';
 import { MdOutlineLanguage } from "react-icons/md";
@@ -24,22 +26,20 @@ async function  Contact({ params: { lng } }) {
 
        <div className=" containerr text-center">
         <span className="text-4xl text-white font-bold">           {t('breadcrumb.title')}</span>
-<nav class="flex pt-5 justify-center" aria-label="Breadcrumb ">
+<nav className="flex pt-5 justify-center" aria-label="Breadcrumb ">
 
-  <ol class="inline-flex px-2 items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
-    <li class="inline-flex items-cente ">
-      <Link href={`/${lng}`} class="flex gap-2 items-center text-xl font-medium text-white hover:text-blue-200 dark:text-gray-400 dark:hover:text-white">
+  <ol className="inline-flex px-2 items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+    <li className="inline-flex items-cente ">
+      <Link href={`/${lng}`} className="flex gap-2 items-center text-xl font-medium text-white hover:text-blue-200 dark:text-gray-400 dark:hover:text-white">
      <TiHome/>
           {t('breadcrumb.home')}
 
       </Link>
     </li>
     <li>
-      <div class="flex items-center">
-        <svg class="rtl:rotate-180 w-3 h-3  mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-        </svg>
-        <a href="#" class="ms-1 text-xl font-medium text-white hover:text-blue-200  md:ms-2 dark:text-gray-400 dark:hover:text-white">          {t('breadcrumb.hotelBooking')}</a>
+      <div className="flex items-center">
+       <IoIosArrowForward/>
+        <a href="#" className="ms-1 text-xl font-medium text-white hover:text-blue-200  md:ms-2 dark:text-gray-400 dark:hover:text-white">          {t('breadcrumb.hotelBooking')}</a>
       </div>
     </li>
  

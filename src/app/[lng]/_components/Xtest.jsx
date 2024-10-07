fc6752e3e70162f1a01ef645ebed34c172e663df
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-10-07 10:53:16 +0300

    enhance loading compoent

--- src/app/[lng]/_components/Xtest.jsx
+++ src/app/[lng]/_components/Xtest.jsx
@@ -1,41 +1,51 @@
 'use client'; 
-
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function ClientComponent() {
-  const [loading, setLoading] = useState('false');
+  const [isLoading, setIsLoading] = useState(true);
 
-    
   useEffect(() => {
-      setLoading("true")
-//       sessionStorage.setItem('isLoaded', 'true');
- 
+    const handleContentLoaded = () => {
+      setIsLoading(false); 
+    };
 
-  }, []); 
-//          if (localStorage.getItem('isLoaded')=="true") {
-//    return ;
-//   }
-//      if (sessionStorage.getItem('isLoaded')=="true") {
-//    return ;
-//   }
+    if (document.readyState === 'complete') {
+        console.log("hi");
+      setIsLoading(false);
+    } else {
+      window.addEventListener('load', handleContentLoaded);
+    }
+
+    // Cleanup the event listener on unmount
+    return () => {
+      window.removeEventListener('load', handleContentLoaded);
+    };
+  }, []);
+
   return (
-        <div className={`  ${loading=="true"?"hidden":"flex"}  fixed z-[400] layout-loader bg-[#213d52]   items-center justify-center overflow-hidden`}>
+    <div>
+      {isLoading ? (
+         <div className={`  flex fixed z-[400] layout-loader bg-[#213d52]   items-center justify-center overflow-hidden`}>
             <div className="containerr flex flex-col items-center gap-3">
-            
+           
              <div className="loader flex justify-center items-center">
-                            <div className="company-logo  z-20"></div>
+                          <div className="company-logo  z-20"></div>
 
-                            </div>
-                            <div className="loader2"></div>
-            
+                           </div>
+                           <div className="loader2"></div>
+           
             </div>
-                          
+                         
 
                             </div>
+      ) : (
+        <div>
+          <h1>All content loaded!</h1>
+          {/* Rest of your page content */}
+        </div>
+      )}
+    </div>
   );
 }
-
-
-
 
 
